ee115360b7096404f3982c6266bb2d8e9c252cf6
mdbjax <matt.bradley@jax.org>
2021-05-13 18:05:57 -0400

    Edited run.bash script to use slurm

--- run.bash
+++ run.bash
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-#$ -t 1
-#$ -V
-#$ -j y
-#$ -cwd
-#$ -o /dev/null
-#$ -e /dev/null
 
 # An example bash script demonstrating how to run the entire snakemake pipeline
 # This script creates two separate log files:
@@ -19,7 +13,7 @@
 
 # you should specify a directory for all output here, rather than in the config
 # this will override whichever output directory appears in the config
-out_path="out"
+out_path="results"
 mkdir -p "$out_path"
 
 # clear leftover log files
@@ -30,23 +24,29 @@ if [ -f "$out_path/qlog" ]; then
 	echo ""> "$out_path/qlog";
 fi
 
+snakemake --cluster "sbatch -q batch -c 1 --mem=4G -t 24:00:00 -o $out_path/qlog" \
+	 --config out="$out_path" \
+	--latency-wait 60 -k -j \
+	--use-conda "$@" 2>"$out_path/log" >"$out_path/qlog"
+
+
 # check: should we execute via qsub?
-if [[ $* == *--sge-cluster* ]]; then
-	snakemake \
-	--cluster "qsub -t 1 -V -j y -cwd -o $out_path/qlog" \
-	--config out="$out_path" \
-	--latency-wait 60 \
-	--use-conda \
-	-k \
-	-j \
-	${@//--sge-cluster/} &>"$out_path/log"
-else
-	snakemake \
-	--config out="$out_path" \
-	--latency-wait 60 \
-	--use-conda \
-	-k \
-	-j \
-	"$@" 2>"$out_path/log" >"$out_path/qlog"
-fi
+#if [[ $* == *--sge-cluster* ]]; then
+#	snakemake \
+#	--cluster "qsub -t 1 -V -j y -cwd -o $out_path/qlog" \
+#	--config out="$out_path" \
+#	--latency-wait 60 \
+#	--use-conda \
+#	-k \
+#	-j \
+#	${@//--sge-cluster/} &>"$out_path/log"
+#else
+#	snakemake \
+#	--config out="$out_path" \
+#	--latency-wait 60 \
+#	--use-conda \
+#	-k \
+#	-j \
+#	"$@" 2>"$out_path/log" >"$out_path/qlog"
+#fi
 
